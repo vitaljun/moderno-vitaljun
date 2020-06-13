c45b2ebcfff5f80d0ea5e55e633f9d25c0f6410b
vitaljun <vitosjun@gmail.com>
2020-06-13 19:09:53 +0400

    problem in slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
 $(function () {
+
+  $('.rate-star').rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
+
+  $('.product-slider__inner').slick({
+
+  });
   var mixer = mixitup('.products__inner-box');
 
 });

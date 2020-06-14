ae0aeb0c6d0bd84c8d7a9d815279f094b2f61731
vitaljun <vitosjun@gmail.com>
2020-06-14 16:05:11 +0400

    problem with slider-2

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,10 @@ $(function () {
   });
 
   $('.product-slider__inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: false
 
   });
   var mixer = mixitup('.products__inner-box');

502c45fdf8da3601f102eff78c05f8635789665a
vitaljun <vitosjun@gmail.com>
2020-06-24 23:14:18 +0400

    problem with display flex

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,16 @@ $(function () {
     arrows: false
 
   });
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+  });
+
   var mixer = mixitup('.products__inner-box');
 
 });

c1569693e2c4b093f574e5f8405d4c237218f327
vitaljun <vitosjun@gmail.com>
2020-07-12 14:45:34 +0400

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,3 @@
-const {
-  on
-} = require("gulp");
-
 $(function () {
 
   $('.rate-star').rateYo({
@@ -27,7 +23,28 @@ $(function () {
     prefix: "$"
   });
 
+  $(".icon-th-list").on("click", function () {
+    $(".product__item").addClass("list");
+    $(".icon-th-list").addClass("active");
+    $(".icon-th-large").removeClass("active");
+  });
+  $(".icon-th-large").on("click", function () {
+    $(".product__item").removeClass("list");
+    $(".icon-th-large").addClass("active");
+    $(".icon-th-list").removeClass("active");
+  });
 
-  var mixer = mixitup('.products__inner-box');
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').slideToggle();
+  });
+  $('.header__btn-menu').on('click', function () {
+    $('.header__box').toggleClass('active');
+  });
+
+
+
+  if ($('.products__inner-box').length) {
+    var mixer = mixitup('.products__inner-box');
+  }
 
 });
